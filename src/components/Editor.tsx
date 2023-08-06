34b5fa82d4441ac73c86e1840a92dfc23af011b4
Jaechan <dlwocks31@gmail.com>
2023-08-06 19:55:22 +0900

    imageblock demo

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,12 +1,86 @@
-
-import { BlockNoteEditor } from "@blocknote/core";
+import { BlockSchema, defaultBlockSchema, defaultProps } from "@blocknote/core";
 import "@blocknote/core/style.css";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import {
+  BlockNoteView,
+  InlineContent,
+  ReactSlashMenuItem,
+  createReactBlockSpec,
+  getDefaultReactSlashMenuItems,
+  useBlockNote,
+} from "@blocknote/react";
+import { RiImage2Fill } from "react-icons/ri";
 
-// Our <Editor> component that we can now use
-export default function Editor() {
+export default function App() {
+  // Creates a custom image block.
+  const ImageBlock = createReactBlockSpec({
+    type: "image",
+    propSchema: {
+      ...defaultProps,
+      src: {
+        default: "https://via.placeholder.com/1000",
+      },
+      alt: {
+        default: "image",
+      },
+    },
+    containsInlineContent: true,
+    render: ({ block }) => (
+      <div id="image-wrapper">
+        <img
+          src={block.props.src}
+          alt={block.props.alt}
+          contentEditable={false}
+        />
+        <InlineContent />
+      </div>
+    ),
+  });
+
+  // The custom schema, which includes the default blocks and the custom image
+  // block.
+  const customSchema = {
+    // Adds all default blocks.
+    ...defaultBlockSchema,
+    // Adds the custom image block.
+    image: ImageBlock,
+  } satisfies BlockSchema;
+
+  // Creates a slash menu item for inserting an image block.
+  const insertImage: ReactSlashMenuItem<typeof customSchema> = {
+    name: "Insert Image",
+    execute: (editor) => {
+      const src: string | null = prompt("Enter image URL");
+      const alt: string | null = prompt("Enter image alt text");
+
+      editor.insertBlocks(
+        [
+          {
+            type: "image",
+            props: {
+              src: src || "https://via.placeholder.com/1000",
+              alt: alt || "image",
+            },
+          },
+        ],
+        editor.getTextCursorPosition().block,
+        "after",
+      );
+    },
+    aliases: ["image", "img", "picture", "media"],
+    group: "Media",
+    icon: <RiImage2Fill />,
+    hint: "Insert an image",
+  };
+
   // Creates a new editor instance.
-  const editor: BlockNoteEditor | null = useBlockNote({});
+  const editor = useBlockNote({
+    // Tells BlockNote which blocks to use.
+    blockSchema: customSchema,
+    slashMenuItems: [
+      ...getDefaultReactSlashMenuItems(customSchema),
+      insertImage,
+    ],
+  });
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
